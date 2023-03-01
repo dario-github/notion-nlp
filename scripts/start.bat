38e38e01c2b15917adbe20a8d675d50ae2220183
Dario Zhang <zdclink@gmail.com>
2023-03-02 06:17:55 +0800

    :bug: fix unreadable bat code

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,15 +1,16 @@
+chcp 65001 > nul 2>&1
 @echo off
 
 color 0a
 :main
 echo 1. English
-echo 2. ¼òÌåÖĞÎÄ
+echo 2. ç®€ä½“ä¸­æ–‡
 echo=
-set /p language=Choose Language(Enter the number) / Ñ¡ÔñÓïÑÔ (ÊäÈëĞòºÅ):
+set /p language=Choose Language(Enter the number) / é€‰æ‹©è¯­è¨€ (è¾“å…¥åºå·):
 
 if %language% == 1 (start English.bat)
 if %language% == 2 (start Chinese-simple.bat)
 
-echo Please choose 1 or 2 / ÇëÑ¡Ôñ1»ò2
+echo Please choose 1 or 2 / è¯·é€‰æ‹© 1 æˆ– 2 
 cls
 goto main
