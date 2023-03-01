c342c2f0865da36724ef728b459d343b0f3ccf43
Dario Zhang <zdclink@gmail.com>
2023-03-02 06:01:18 +0800

    :bug: fix unreadable bat code

--- scripts/Chinese-simple.bat
+++ scripts/Chinese-simple.bat
@@ -1,28 +1,28 @@
+chcp 65001 > nul 2>&1
 @echo off
-
 color 0a
-:: 首次执行脚本：下载资源文件
+:: 首次执行脚本：下载资源文件 
 if not exist ".\Temp-dataset\configs\config.yaml" (
-    echo 检测到脚本首次执行，将下载资源文件并执行引导，请耐心等待 1-3 min...
+    echo 检测到脚本首次执行，将下载资源文件并执行引导，请耐心等待 1-3 min... 
     .\notion-nlp-win64.exe first-try
     IF NOT %ERRORLEVEL% EQU 0 (
       echo "发现未知错误...请责令作者处理，记得复制上面的错误日志哦~到这里粘贴日志 ==> https://reurl.cc/b7nDkl"
-      set /p tmp=请按任意键退出脚本...
+      set /p tmp=请按任意键退出脚本... 
       exit)
-    echo 样例任务已执行完毕，请按回车键查看词云图样例及目录
-    set /p tmp=查看样例效果后请返回本窗口，继续下一步骤...
+    echo 样例任务已执行完毕，请按回车键查看词云图样例及目录 
+    set /p tmp=查看样例效果后请返回本窗口，继续下一步骤... 
     start "" ".\Temp-dataset\results\wordcloud\chinese-simple_task"
     echo=
-    set /p tmp=请按回车键查看主题总结markdown文档...
+    set /p tmp=请按回车键查看主题总结markdown文档... 
     start "" ".\Temp-dataset\results\tfidf_analysis\chinese-simple_task\chinese-simple_task.top_5.md"
     echo=
     echo "如何配置自己的任务？教程指引 ==> https://reurl.cc/NqAybp"
     echo=
-    set /p tmp=请按回车键打开参数文件，开始配置您自己的任务...
+    set /p tmp=请按回车键打开参数文件，开始配置您自己的任务... 
     copy /y ".\Temp-dataset\configs\config.test.yaml" ".\Temp-dataset\configs\config.yaml" > nul
     start "" ".\Temp-dataset\configs\config.yaml"
     echo=
-    echo /p tmp=请按回车键进入主菜单...
+    echo /p tmp=请按回车键进入主菜单... 
     cls
 ) else (
     echo=
@@ -31,24 +31,23 @@ if not exist ".\Temp-dataset\configs\config.yaml" (
 color 0a
 :main
 color 0a
-echo=
 echo ================== ☆ Notion 自然语言处理 ☆ ======================
 echo=
-echo 作者:  Dario Zhang
-echo 版本:  v1.0.7.2
-echo 代码:  https://github.com/dario-github/notion-nlp
-echo 描述:  从Notion数据库中读取文本并进行自然语言处理分析
+echo 作者:  Dario Zhang 
+echo 版本:  v1.0.7.2 
+echo 代码:  https://github.com/dario-github/notion-nlp 
+echo 描述:  从Notion数据库中读取文本并进行自然语言处理分析 
 echo=
 echo =========================   推广区   ============================
 echo=                                   
-echo 激励作者:  https://reurl.cc/7R3MeN
+echo 激励作者:  https://reurl.cc/7R3MeN 
 echo=
 echo ===============================================================
 echo=
-echo 1. 查看任务信息
-echo 2. 运行所有任务
-echo 3. 运行指定任务
-echo 4. 观赏幸运鹦鹉
+echo 1.查看任务信息 
+echo 2.运行所有任务 
+echo 3.运行指定任务 
+echo 4.观赏幸运鹦鹉 
 
 echo=
 set /p opt=选项 (输入序号):
@@ -58,45 +57,45 @@ if %opt% == 2 goto two
 if %opt% == 3 goto three
 if %opt% == 4 goto lastopt
 
-echo 无效选项
+echo 无效选项 
 cls
 goto main
 
-:: 查看任务信息
+:: 查看任务信息 
 :one
-.\notion-nlp-win64.exe task-info --config-file ".\Temp-dataset\configs\config.yaml"
+.\notion-nlp-win64.exe task-info --config-file .\Temp-dataset\configs\config.yaml
 IF %ERRORLEVEL% EQU 0 GOTO endresult
-echo 未找到参数文件或配置错误，请按照教程检查配置文件
+echo 未找到参数文件或配置错误，请按照教程检查配置文件 
 goto main
 
-:: 执行所有任务
+:: 执行所有任务 
 :two
-.\notion-nlp-win64.exe run-all-tasks --config-file ".\Temp-dataset\configs\config.yaml"
+.\notion-nlp-win64.exe run-all-tasks --config-file .\Temp-dataset\configs\config.yaml
 IF %ERRORLEVEL% EQU 0 GOTO endresult
-echo 未找到参数文件或配置错误，请按照教程检查配置文件
+echo 未找到参数文件或配置错误，请按照教程检查配置文件 
 goto main
 
-:: 执行单个任务
+:: 执行单个任务 
 :three
 set /p name=请输入参数文件中的任务名，如包含空格，请用双引号包裹任务名，输入 info 查看所有任务信息: 
 if not defined name goto three
 if %name% == info goto one
 .\notion-nlp-win64.exe run-task --task-name %name% --config-file ".\Temp-dataset\configs\config.yaml"
 IF %ERRORLEVEL% EQU 0 GOTO endresult
-echo 未找到参数文件或配置错误，请按照教程检查配置文件
+echo 未找到参数文件或配置错误，请按照教程检查配置文件 
 goto main
 
 
 :endresult
 echo=
-echo  ██████╗ ██╗  ██╗
-echo ██╔═══██╗██║ ██═╝
-echo ██║   ██║████║
-echo ██║   ██║██║ ██╗
-echo ╚██████╔╝██║╗ ██╗
-echo  ╚═════╝ ╚══╝ ╚═╝
+echo  ██████╗ ██╗  ██╗ 
+echo ██╔═══██╗██║ ██═╝ 
+echo ██║   ██║████║ 
+echo ██║   ██║██║ ██╗ 
+echo ╚██████╔╝██║╗ ██╗ 
+echo  ╚═════╝ ╚══╝ ╚═╝ 
 echo=
-set /p tmp=执行完毕，请按回车键返回菜单...
+set /p tmp=执行完毕，请按回车键返回菜单... 
 cls
 goto main
 
--- scripts/English.bat
+++ scripts/English.bat
@@ -1,29 +1,29 @@
+chcp 65001 > nul 2>&1
 @echo off
-
 color 0a
 :: First run of the script: download resource files
 if not exist ".\Temp-dataset\configs\config.yaml" (
-echo Detected first run of the script, will download resource files and execute the guide, please wait patiently for 1-3 min...
+echo Detected first run of the script, will download resource files and execute the guide, please wait patiently for 1-3 min... 
 .\notion-nlp-win64.exe first-try
 IF NOT %ERRORLEVEL% EQU 0 (
-echo "Unknown error found...Please ask the author to handle it, and remember to copy the error log above~Paste the log here ==> https://reurl.cc/b7nDkl"
-set /p tmp=Press any key to exit the script...
+echo "Unknown error found...Please ask the author to handle it, and remember to copy the error log above~Paste the log here ==> https://reurl.cc/b7nDkl" 
+set /p tmp=Press any key to exit the script... 
 exit)
-echo Example task has been executed, please press Enter to see the word cloud example and directory
-set /p tmp=After viewing the sample effect, please return to this window to continue to the next step...
+echo Example task has been executed, please press Enter to see the word cloud example and directory 
+set /p tmp=After viewing the sample effect, please return to this window to continue to the next step... 
 :: start "" ".\Temp-dataset\results\wordcloud\chinese-simple_task\colormap_viridis.png"
 start "" ".\Temp-dataset\results\wordcloud\chinese-simple_task"
 echo=
-set /p tmp=Press Enter to view the summary markdown document of the topic...
+set /p tmp=Press Enter to view the summary markdown document of the topic... 
 start "" ".\Temp-dataset\results\tfidf_analysis\chinese-simple_task\chinese-simple_task.top_5.md"
 echo=
-echo "How to configure your own task? Tutorial guidance ==> https://reurl.cc/NqAybp"
+echo "How to configure your own task? Tutorial guidance ==> https://reurl.cc/NqAybp" 
 echo=
-set /p tmp=Press Enter to open the parameter file and start configuring your own task...
+set /p tmp=Press Enter to open the parameter file and start configuring your own task... 
 copy /y ".\Temp-dataset\configs\config.test.yaml" ".\Temp-dataset\configs\config.yaml" > nul
 start "" ".\Temp-dataset\configs\config.yaml"
 echo=
-echo /p tmp=Press Enter to enter the main menu...
+echo /p tmp=Press Enter to enter the main menu... 
 cls
 ) else (
 echo=
@@ -47,57 +47,57 @@ echo Support the author: https://reurl.cc/7R3MeN
 echo=
 echo ===================================================================
 echo=
-echo 1. View task information
-echo 2. Run all tasks
-echo 3. Run a specific task
-echo 4. Watch the Lucky Parrot
+echo 1. View task information 
+echo 2. Run all tasks 
+echo 3. Run a specific task 
+echo 4. Watch the Lucky Parrot 
 
 echo=
-set /p opt=Option (Enter the number):
+set /p opt=Option (Enter the number): 
 
 if %opt% == 1 goto one
 if %opt% == 2 goto two
 if %opt% == 3 goto three
 if %opt% == 4 goto lastopt
 
-echo Invalid option
+echo Invalid option 
 cls
 goto main
 
-:: View task information
+:: View task information 
 :one
 .\notion-nlp-win64.exe task-info --config-file ".\Temp-dataset\configs\config.yaml"
 IF %ERRORLEVEL% EQU 0 GOTO endresult
-echo Parameter file not found or configuration error, please check the configuration file according to the tutorial
+echo Parameter file not found or configuration error, please check the configuration file according to the tutorial 
 goto main
 
-:: Execute all tasks
+:: Execute all tasks 
 :two
 .\notion-nlp-win64.exe run-all-tasks --config-file ".\Temp-dataset\configs\config.yaml"
 IF %ERRORLEVEL% EQU 0 GOTO endresult
-echo Parameter file not found or configuration error, please check the configuration file according to the tutorial
+echo Parameter file not found or configuration error, please check the configuration file according to the tutorial 
 goto main
 
-:: Execute a single task
+:: Execute a single task 
 :three
-set /p name=Please enter the task name from the parameter file. If the name contains spaces, please enclose it in double quotes. Enter "info" to view all task information:
+set /p name=Please enter the task name from the parameter file. If the name contains spaces, please enclose it in double quotes. Enter "info" to view all task information: 
 if not defined name goto three
 if %name% == info goto one
 .\notion-nlp-win64.exe run-task --task-name %name% --config-file ".\Temp-dataset\configs\config.yaml"
 IF %ERRORLEVEL% EQU 0 GOTO endresult
-echo Parameter file not found or configuration error. Please check the configuration file according to the tutorial.
+echo Parameter file not found or configuration error. Please check the configuration file according to the tutorial. 
 goto main
 
 :endresult
 echo=
-echo  ██████╗ ██╗  ██╗
-echo ██╔═══██╗██║ ██═╝
-echo ██║   ██║████║
-echo ██║   ██║██║ ██╗
-echo ╚██████╔╝██║╗ ██╗
-echo  ╚═════╝ ╚══╝ ╚═╝
+echo  ██████╗ ██╗  ██╗ 
+echo ██╔═══██╗██║ ██═╝ 
+echo ██║   ██║████║ 
+echo ██║   ██║██║ ██╗ 
+echo ╚██████╔╝██║╗ ██╗ 
+echo  ╚═════╝ ╚══╝ ╚═╝ 
 echo=
-set /p tmp=Execution completed. Press Enter to return to the menu...
+set /p tmp=Execution completed. Press Enter to return to the menu... 
 cls
 goto main
 
