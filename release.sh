b0d0744e4c662dd6cd01da4bf7dbbaf6529b013b
Dario Zhang <zdclink@gmail.com>
2023-03-02 08:04:22 +0800

    :arrow_up: CI config

--- release.sh
+++ release.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# dir name
+dir=$(basename $(dirname $0))
+
 # pypi token
 pypi_token=$(cat PYPI_TOKEN)
 
 # 从__init__.py文件中读取版本信息
-version=$(python -c "from src.notion_nlp import __version__; print(__version__)")
+version=$(python -c "from src.$dir import __version__; print(__version__)")
 
 # build & publish
 poetry build && \
 poetry publish -u __token__ -p $pypi_token
 
 # pyinstaller linux exec file
-pyinstaller -F src/notion_nlp/__main__.py -n notion-nlp-linux && \
+pyinstaller -F src/$dir/__main__.py -n $dir-linux && \
 
 # Compress the executable file
-zip ./dist/notion-nlp-linux.zip ./dist/notion-nlp-linux ./scripts/Chinese-simple.sh scripts/English.sh scripts/start.sh
+zip ./dist/$dir-linux.zip ./dist/$dir-linux ./scripts/Chinese-simple.sh scripts/English.sh scripts/start.sh
 
-zip dist/notion-nlp-win64.zip dist/notion-nlp-win64.exe scripts/Chinese-simple.bat scripts/English.bat scripts/start.bat
+zip dist/$dir-win64.zip dist/$dir-win64.exe scripts/Chinese-simple.bat scripts/English.bat scripts/start.bat
